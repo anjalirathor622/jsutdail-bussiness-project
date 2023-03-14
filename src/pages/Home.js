b73eb7619b90ce85f8588910ec36bef7dbe31834
anjalirathor622 <anjalirathor622@gmail.com>
2023-03-15 04:45:03 +0530

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 //import area
 import React, { useEffect, useState } from 'react'
-import URL from '../Helper/url';
 
 //function area
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
   const[businessCategory,setBusinessCatagory] = useState([]);
 
   useEffect(()=>{
-    fetch(`${URL}/api/bussiness-names?populate=*`)
+    fetch(`http://localhost:1337/api/bussiness-names?populate=*`)
     .then(res=>res.json())
     .then((data)=>{
       console.log(data.data);
@@ -27,12 +26,12 @@ export default function Home() {
       <ul className='nav'>
         {
           businessCategory.map((cv,idx,arr)=>{
-            return  (<li key={idx} className='m-3'>
+            return  <li key={idx} className='me-3'>
                       <a href='#' className='text-center'>
-                        <img src={`${URL}`+cv.attributes.logo.data.attributes.url}  /><br/>
+                        <img src={'http://localhost:1337'+cv.attributes.logo.data.attributes.url} /><br/>
                         {cv.attributes.name}
                       </a>
-                    </li>)
+                    </li>
           })
         }
         
